0c8d53c8bd43d15c541fdbdc64f993d700dbe978
kshitijapatil31 <72263342+kshitijapatil31@users.noreply.github.com>
2021-08-08 20:55:01 +0530

    Add files via upload

---
+++ PracticeProblem1.java
@@ -0,0 +1,40 @@
+
+public class PracticeProblem1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison");
+		
+		int x1=5,x2=9,y1=5,y2=9;
+		double length1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println(length1);
+		int a1=12,a2=6,b1=12,b2=5;
+		double length2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+		System.out.println(length2);
+		
+	    Double d1=new Double(length1);
+	    Double d2=new Double(length2);
+	    String l1=d1.toString();
+	    String l2=d1.toString();
+	    
+	    /* ----equality----- */
+	    
+		if(l2.equals(l1)) {
+			System.out.println("equal");
+		}else {
+			System.out.println(" not equal");
+		}
+		
+		/* ----comparison----- */
+		
+		System.out.println(l1.compareTo(l2));
+		if(length1>length2) {
+			System.out.println("length1 is greater than length2 ");
+		}else if(length1<length2){
+			System.out.println("length1 is smaller than length2 ");
+		}else {
+			System.out.println("length1 equals to length2 ");
+		}
+		
+	}
+
+}
